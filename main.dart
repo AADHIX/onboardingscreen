829e2ff21a7dde89d74970b319c76ec83033cef4
ADARSH AJAY <99534398+AADHIX@users.noreply.github.com>
2025-01-20 12:43:41 -0600

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,7 +2,172 @@ import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 void main() {
+  runApp(const MyApp());import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: OnboardingScreen(),
+    );
+  }
+}
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  OnboardingScreenState createState() => OnboardingScreenState();
+}
+
+class OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _pageController = PageController(initialPage: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            controller: _pageController,
+            children: List.generate(6, (index) => buildPage(index)),
+          ),
+          Positioned(
+            top: 20,
+            right: 20,
+            child: TextButton(
+              onPressed: () {
+                _pageController.jumpToPage(5);
+              },
+              child: const Text(
+                'SKIP',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            left: 20,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () {
+                if (_pageController.page! > 0) {
+                  _pageController.previousPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                }
+              },
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_forward, color: Colors.white),
+              onPressed: () {
+                if (_pageController.page! < 5) {
+                  _pageController.nextPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                }
+              },
+            ),
+          ),
+          Positioned(
+            bottom: 100,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: SmoothPageIndicator(
+                controller: _pageController,
+                count: 6,
+                effect: const WormEffect(
+                  dotHeight: 12,
+                  dotWidth: 12,
+                  activeDotColor: Colors.white,
+                  dotColor: Colors.yellow,
+                ),
+                onDotClicked: (index) {
+                  _pageController.animateToPage(
+                    index,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.easeInOut,
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildPage(int index) {
+    return Stack(
+      children: [
+        Container(
+          color: Colors.primaries[index % Colors.primaries.length],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/image${index + 1}.png', height: 200),
+              const SizedBox(height: 20),
+              Text(
+                'Page ${index + 1}',
+                style: const TextStyle(fontSize: 24, color: Colors.white),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                color: Colors.black.withOpacity(0.5),
+                child: Text(
+                  'This is a description for page ${index + 1}',
+                  style: const TextStyle(fontSize: 16, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 80,
+            color: Colors.black.withOpacity(0.5),
+            child: Center(
+              child: SmoothPageIndicator(
+                controller: _pageController,
+                count: 6,
+                effect: const WormEffect(
+                  dotHeight: 12,
+                  dotWidth: 12,
+                  activeDotColor: Colors.white,
+                  dotColor: Colors.grey,
+                ),
+                onDotClicked: (index) {
+                  _pageController.animateToPage(
+                    index,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.easeInOut,
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
   
 }
 
